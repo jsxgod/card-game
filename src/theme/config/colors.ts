98d9b814e1e6f1db8eba1196e175916ffd3971a6
Kacper Smyczyk <smykoo7@gmail.com>
2022-05-09 12:29:16 +0200

    Add color codes

--- src/theme/config/colors.ts
+++ src/theme/config/colors.ts
@@ -1,3 +1,16 @@
-const defaultColors = {};
+const defaultColors = {
+  pureWhite: "#FFFFFF",
+  white: "#EEEEEE",
+  secondaryWhite: "#F0F0F0",
+  pureBlack: "#000000",
+  black: "#212121",
+  dark: "#080808",
+  red: "#CD1717",
+  darkRed: "#4E0000",
+  cardRed: "#F50206",
+  cardBlue: "#000080",
+  lightGray: "#E9E9E9",
+  gray: "#E5E5E5",
+};
 
 export default defaultColors;
