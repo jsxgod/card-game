b56b0d65bc1501f48a6cc7900cce7d931591204c
Kacper Smyczyk <smykoo7@gmail.com>
2022-05-09 10:59:48 +0200

    Create starter theme

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,6 +1,27 @@
 import { DefaultTheme } from "styled-components";
+import {
+  fonts,
+  fontSizes,
+  lineHeights,
+  fontWeights,
+  textStyles,
+} from "./config/typography";
+import breakpoints from "./config/breakpoints";
+import sizes from "./config/sizes";
+import space from "./config/space";
+import defaultColors from "./config/colors";
 
-const theme = { fonts: { body: "Times New Roman" } };
+const theme = {
+  breakpoints,
+  fonts,
+  fontSizes,
+  fontWeights,
+  lineHeights,
+  textStyles,
+  colors: {},
+  space,
+  sizes,
+};
 
 export type Theme = typeof theme & DefaultTheme;
 
