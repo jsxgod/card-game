f1a8fcbf90c6802c78e6731424420b8d6cdb7fd4
Kacper Smyczyk <smykoo7@gmail.com>
2022-05-11 21:17:39 +0200

    Create the /about page

---
+++ pages/about/index.tsx
@@ -0,0 +1,28 @@
+import {
+  HandwrittenHeader,
+  Header,
+} from "../../src/components/atoms/Typography/Headers";
+import { MainGridBox } from "../../src/components/layout/grid";
+import styles from "../../src/sass/components/About.module.scss";
+
+export default function AboutPage() {
+  return (
+    <MainGridBox>
+      <div className={styles.wrapper}>
+        <Header as="h2" content={`Hope You enjoyed your stay on the`} />
+      </div>
+      <div className={styles.wrapper}>
+        <Header as="h2" content="site" />
+      </div>
+      <div className={styles.wrapper}>
+        <Header as="h2" content="by" />
+      </div>
+      <div className={styles.wrapper}>
+        <HandwrittenHeader as="h1" content="Kacper" />
+      </div>
+      <div className={styles.wrapper}>
+        <HandwrittenHeader as="h1" content="Smyczyk" />
+      </div>
+    </MainGridBox>
+  );
+}
