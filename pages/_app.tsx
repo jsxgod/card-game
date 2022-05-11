c1b5017d4a0207fbf2585335cc4dbd0b7786360c
Kacper Smyczyk <smykoo7@gmail.com>
2022-05-11 23:18:26 +0200

    Configure a basic redux store

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
 import { AppProps } from "next/app";
 import "../src/sass/main.scss";
+import { store } from "../src/redux/store";
+import { Provider } from "react-redux";
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => (
-  <Component {...pageProps} />
+  <Provider store={store}>
+    <Component {...pageProps} />
+  </Provider>
 );
 
 export default MyApp;
