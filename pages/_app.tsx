f1bb935a2b4f75f75d5ae2c82874e9be8069c96a
Kacper Smyczyk <smykoo7@gmail.com>
2022-05-20 09:36:41 +0200

    Change to normal function

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,12 @@ import "../src/sass/main.scss";
 import { store } from "../src/redux/store";
 import { Provider } from "react-redux";
 
-const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => (
-  <Provider store={store}>
-    <Component {...pageProps} />
-  </Provider>
-);
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
+}
 
 export default MyApp;
