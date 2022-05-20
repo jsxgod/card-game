bc3607f08d67dfd4a57e1a793ecb8ad594b27002
jsxgod <smykoo7@gmail.com>
2022-05-20 16:36:42 +0200

    Change socket initialization

--- src/context/createContext.ts
+++ src/context/createContext.ts
@@ -2,7 +2,7 @@ import { createContext } from "react";
 import { io, Socket } from "socket.io-client";
 import { DefaultEventsMap } from "socket.io/dist/typed-events";
 
-const socket = io();
+const socket = io("/", { transports: ["websocket"], upgrade: false });
 const SocketContext = createContext<Socket<
   DefaultEventsMap,
   DefaultEventsMap
