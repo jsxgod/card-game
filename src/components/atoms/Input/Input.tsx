cf8e8e9632df83be998c279dbab3f194cd8f024d
Kacper Smyczyk <smykoo7@gmail.com>
2022-05-20 09:44:28 +0200

    Create the Input Component

---
+++ src/components/atoms/Input/Input.tsx
@@ -0,0 +1,51 @@
+import { Dispatch, SetStateAction } from "react";
+import styles from "../../../sass/components/Input.module.scss";
+
+interface InputProps {
+  value: string;
+  placeholder?: string;
+  handleOnChange: (param: string) => void;
+  textSize?: TextSize;
+}
+
+type TextSize = "p" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+
+const tagToFontSize = (tag: TextSize): string => {
+  switch (tag) {
+    case "p":
+      return "1rem";
+    case "h1":
+      return "8rem";
+    case "h2":
+      return "6rem";
+    case "h3":
+      return "4rem";
+    case "h4":
+      return "3rem";
+    case "h5":
+      return "2rem";
+    case "h6":
+      return "1.5rem";
+  }
+};
+
+export function Input({
+  value,
+  placeholder = "type here...",
+  textSize = "p",
+  handleOnChange,
+}: InputProps) {
+  return (
+    <input
+      className={styles["custom-input"]}
+      value={value}
+      onChange={(event) => handleOnChange(event.target.value)}
+      type={"text"}
+      style={{ fontSize: tagToFontSize(textSize) }}
+      autoFocus={true}
+      placeholder={placeholder}
+    />
+  );
+}
+
+export default Input;
