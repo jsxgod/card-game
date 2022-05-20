1384e7640060c29ada92cd1dadcf939176565400
Kacper Smyczyk <smykoo7@gmail.com>
2022-05-20 09:54:33 +0200

    Create redux store. Add reducers. Export types.

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,16 @@
 import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
+import { profileReducer } from "./reducers/profileReducer";
+import playerReducer from "./slices/playerSlice";
+import menuReducer from "./slices/menuSlice";
+import gameReducer from "./slices/gameSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    profile: profileReducer,
+    menu: menuReducer,
+    player: playerReducer,
+    game: gameReducer,
+  },
 });
 
 export type AppDispatch = typeof store.dispatch;
