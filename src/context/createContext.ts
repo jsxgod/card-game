6c5789934aee20e0a79761a5b210e10c0a891709
Kacper Smyczyk <smykoo7@gmail.com>
2022-05-20 09:50:01 +0200

    Create context to store user's socket

---
+++ src/context/createContext.ts
@@ -0,0 +1,12 @@
+import { createContext } from "react";
+import { io, Socket } from "socket.io-client";
+import { DefaultEventsMap } from "socket.io/dist/typed-events";
+
+const socket = io();
+const SocketContext = createContext<Socket<
+  DefaultEventsMap,
+  DefaultEventsMap
+> | null>(socket);
+
+export const SocketProvider = SocketContext.Provider;
+export default SocketContext;
