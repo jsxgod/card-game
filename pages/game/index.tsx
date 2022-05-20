a8329af66b16fdc3e696002f8442fdd8f5d91772
Kacper Smyczyk <smykoo7@gmail.com>
2022-05-20 09:38:29 +0200

    Add Game page

---
+++ pages/game/index.tsx
@@ -0,0 +1,10 @@
+import { MainGridBox } from "../../src/components/layout/grid";
+import Game from "../../src/components/organisms/Game/Game";
+
+export default function GamePage() {
+  return (
+    <MainGridBox>
+      <Game />
+    </MainGridBox>
+  );
+}
