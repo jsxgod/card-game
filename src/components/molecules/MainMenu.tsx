d2e3604bc9b088b139fa8db83e2d5b71016ebf93
Kacper Smyczyk <smykoo7@gmail.com>
2022-05-20 09:46:48 +0200

    Move the onClick handler from prop to the component

--- src/components/molecules/MainMenu.tsx
+++ src/components/molecules/MainMenu.tsx
@@ -1,31 +1,51 @@
 import { Dispatch, SetStateAction } from "react";
-import { SubMenuType } from "../../../pages/types";
+import Link from "next/link";
 import styles from "../../sass/components/MainMenu.module.scss";
 import { Header } from "../atoms/Typography/Headers";
+import { useDispatch, useSelector } from "react-redux";
+import { closeSubMenus, openSubMenu } from "../../redux/slices/menuSlice";
+import { RootState } from "../../redux/store";
+import type { SubMenu } from "../../redux/slices/types";
 
-type MainMenuProps = {
-  handleOpenSubmenu: Dispatch<SetStateAction<SubMenuType>>;
-};
+type MainMenuProps = {};
 
-export function MainMenu({ handleOpenSubmenu }: MainMenuProps): JSX.Element {
+export function MainMenu({}: MainMenuProps): JSX.Element {
+  const dispatch = useDispatch();
+  const openedSubMenu = useSelector(
+    (state: RootState) => state.menu.subMenuOpened
+  );
+
+  const handleOpenSubMenu = (subMenu: SubMenu): void => {
+    if (openedSubMenu === subMenu) {
+      dispatch(closeSubMenus());
+    } else {
+      dispatch(openSubMenu(subMenu));
+    }
+  };
+
   return (
     <div className={styles.wrapper}>
-      <div className={styles["item-wrapper"]}>
-        <Header as="h1" content="Play" />
-      </div>
+      <Link href="/play">
+        <div className={styles["item-wrapper"]}>
+          <Header as="h1" content="Play" />
+        </div>
+      </Link>
       <div
         className={styles["item-wrapper"]}
-        onClick={() => handleOpenSubmenu("profile")}
+        onClick={() => handleOpenSubMenu("profile")}
       >
         <Header as="h1" content="Profile" />
       </div>
       <div
         className={styles["item-wrapper"]}
-        onClick={() => handleOpenSubmenu("howto")}
+        onClick={() => handleOpenSubMenu("howto")}
       >
         <Header as="h1" content="How to" />
       </div>
-      <div className={styles["item-wrapper"]}>
+      <div
+        className={styles["item-wrapper"]}
+        onClick={() => handleOpenSubMenu("about")}
+      >
         <Header content="About" as="h3" />
       </div>
     </div>
